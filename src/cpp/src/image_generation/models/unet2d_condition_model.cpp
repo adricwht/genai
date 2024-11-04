RyanMetcalfeInt8/openvino.genai

text2image: add initial pimpl approach for unet2d_condition_model

src/cpp/src/text2image/models/unet2d_condition_model.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "openvino/genai/text2image/unet2d_condition_model.hpp"
+#include "text2image/models/unet_inference_dynamic.hpp"
 
 #include <fstream>
 
@@ -64,41 +65,25 @@ UNet2DConditionModel& UNet2DConditionModel::reshape(int batch_size, int height, 
     height /= m_vae_scale_factor;
     width /= m_vae_scale_factor;
 
-    std::map<std::string, ov::PartialShape> name_to_shape;
-
-    for (auto && input : m_model->inputs()) {
-        std::string input_name = input.get_any_name();
-        name_to_shape[input_name] = input.get_partial_shape();
-        if (input_name == "timestep") {
-            name_to_shape[input_name][0] = 1;
-        } else if (input_name == "sample") {
-            name_to_shape[input_name] = {batch_size, name_to_shape[input_name][1], height, width};
-        } else if (input_name == "time_ids" || input_name == "text_embeds") {
-            name_to_shape[input_name][0] = batch_size;
-        } else if (input_name == "encoder_hidden_states") {
-            name_to_shape[input_name][0] = batch_size;
-            name_to_shape[input_name][1] = tokenizer_model_max_length;
-        }
-    }
-
-    m_model->reshape(name_to_shape);
+    UNetInference::reshape(m_model, batch_size, height, width, tokenizer_model_max_length);
 
     return *this;
 }
 
 UNet2DConditionModel& UNet2DConditionModel::compile(const std::string& device, const ov::AnyMap& properties) {
     OPENVINO_ASSERT(m_model, "Model has been already compiled. Cannot re-compile already compiled model");
-    ov::Core core = utils::singleton_core();
-    ov::CompiledModel compiled_model;
+
+    m_impl = std::make_shared<UNet2DConditionModel::UNetInferenceDynamic>();
+
     std::optional<AdapterConfig> adapters;
     if (auto filtered_properties = extract_adapters_from_properties(properties, &adapters)) {
         adapters->set_tensor_name_prefix(adapters->get_tensor_name_prefix().value_or("lora_unet"));
         m_adapter_controller = AdapterController(m_model, *adapters, device);
-        compiled_model = core.compile_model(m_model, device, *filtered_properties);
+        m_impl->compile(m_model, device, *filtered_properties);
     } else {
-        compiled_model = core.compile_model(m_model, device, properties);
+        m_impl->compile(m_model, device, properties);
     }
-    m_request = compiled_model.create_infer_request();
+
     // release the original model
     m_model.reset();
 
@@ -106,25 +91,21 @@ UNet2DConditionModel& UNet2DConditionModel::compile(const std::string& device, c
 }
 
 void UNet2DConditionModel::set_hidden_states(const std::string& tensor_name, ov::Tensor encoder_hidden_states) {
-    OPENVINO_ASSERT(m_request, "UNet model must be compiled first");
-    m_request.set_tensor(tensor_name, encoder_hidden_states);
+    OPENVINO_ASSERT(m_impl, "UNet model must be compiled first");
+    m_impl->set_hidden_states(tensor_name, encoder_hidden_states);
 }
 
 void UNet2DConditionModel::set_adapters(const std::optional<AdapterConfig>& adapters) {
+    OPENVINO_ASSERT(m_impl, "UNet model must be compiled first");
     if(adapters) {
-        m_adapter_controller.apply(m_request, *adapters);
+        OPENVINO_ASSERT(m_impl, "UNet model must be compiled first");
+        m_impl->set_adapters(m_adapter_controller, *adapters);
     }
 }
 
 ov::Tensor UNet2DConditionModel::infer(ov::Tensor sample, ov::Tensor timestep) {
-    OPENVINO_ASSERT(m_request, "UNet model must be compiled first. Cannot infer non-compiled model");
-
-    m_request.set_tensor("sample", sample);
-    m_request.set_tensor("timestep", timestep);
-
-    m_request.infer();
-
-    return m_request.get_output_tensor();
+    OPENVINO_ASSERT(m_impl, "UNet model must be compiled first. Cannot infer non-compiled model");
+    return m_impl->infer(sample, timestep);
 }
 
 } // namespace genai

src/cpp/src/text2image/models/unet_inference.hpp
@@ -1,0 +1,99 @@
+// Copyright (C) 2023-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "openvino/genai/text2image/unet2d_condition_model.hpp"
+
+namespace ov {
+namespace genai {
+
+//REMOVE THIS
+static inline void logBasicModelInfo(const std::shared_ptr<ov::Model>& model) {
+    std::cout << "Model name: " << model->get_friendly_name() << std::endl;
+
+    // Dump information about model inputs/outputs
+    ov::OutputVector inputs = model->inputs();
+    ov::OutputVector outputs = model->outputs();
+
+    std::cout << "\tInputs: " << std::endl;
+    for (const ov::Output<ov::Node>& input : inputs) {
+        const std::string name = input.get_any_name();
+        const ov::element::Type type = input.get_element_type();
+        const ov::PartialShape shape = input.get_partial_shape();
+        const ov::Layout layout = ov::layout::get_layout(input);
+
+        std::cout << "\t\t" << name << ", " << type << ", " << shape << ", " << layout.to_string() << std::endl;
+    }
+
+    std::cout << "\tOutputs: " << std::endl;
+    for (const ov::Output<ov::Node>& output : outputs) {
+        const std::string name = output.get_any_name();
+        const ov::element::Type type = output.get_element_type();
+        const ov::PartialShape shape = output.get_partial_shape();
+        const ov::Layout layout = ov::layout::get_layout(output);
+
+        std::cout << "\t\t" << name << ", " << type << ", " << shape << ", " << layout.to_string() << std::endl;
+    }
+
+    return;
+}
+
+class UNet2DConditionModel::UNetInference {
+
+public:
+    virtual void compile(std::shared_ptr<ov::Model> model, const std::string& device, const ov::AnyMap& properties) = 0;
+    virtual void set_hidden_states(const std::string& tensor_name, ov::Tensor encoder_hidden_states) = 0;
+    virtual void set_adapters(AdapterController& adapter_controller, const AdapterConfig& adapters) = 0;
+    virtual ov::Tensor infer(ov::Tensor sample, ov::Tensor timestep) = 0;
+
+    // utility function to resize model given optional dimensions.
+    static void reshape(std::shared_ptr<ov::Model> model,
+                        std::optional<int> batch_size = {},
+                        std::optional<int> height = {},
+                        std::optional<int> width = {},
+                        std::optional<int> tokenizer_model_max_length = {})
+    {
+        std::map<std::string, ov::PartialShape> name_to_shape;
+        for (auto&& input : model->inputs()) {
+            std::string input_name = input.get_any_name();
+            name_to_shape[input_name] = input.get_partial_shape();
+            if (input_name == "timestep") {
+                name_to_shape[input_name][0] = 1;
+            } else if (input_name == "sample") {
+                if (batch_size) {
+                    name_to_shape[input_name][0] = *batch_size;
+                }
+
+                if (height) {
+                    name_to_shape[input_name][2] = *height;
+                }
+
+                if (width) {
+                    name_to_shape[input_name][3] = *width;
+                }
+            } else if (input_name == "time_ids" || input_name == "text_embeds") {
+                if (batch_size) {
+                    name_to_shape[input_name][0] = *batch_size;
+                }
+            } else if (input_name == "encoder_hidden_states") {
+                if (batch_size) {
+                    name_to_shape[input_name][0] = *batch_size;
+                }
+
+                if (tokenizer_model_max_length) {
+                    name_to_shape[input_name][1] = *tokenizer_model_max_length;
+                }
+            } else if (input_name == "timestep_cond") {
+                if (batch_size) {
+                    name_to_shape[input_name][0] = *batch_size;
+                }
+            }
+        }
+
+        model->reshape(name_to_shape);
+    }
+};
+
+}  // namespace genai
+}  // namespace ov

src/cpp/src/text2image/models/unet_inference_dynamic.hpp
@@ -1,0 +1,57 @@
+// Copyright (C) 2023-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "text2image/models/unet_inference.hpp"
+#include "lora_helper.hpp"
+#include "utils.hpp"
+
+namespace ov {
+namespace genai {
+
+
+class UNet2DConditionModel::UNetInferenceDynamic : public UNet2DConditionModel::UNetInference {
+
+public:
+
+    virtual void compile(std::shared_ptr<ov::Model> model, const std::string& device, const ov::AnyMap& properties) override
+    {
+        ov::Core core = utils::singleton_core();
+
+        ov::CompiledModel compiled_model = core.compile_model(model, device, properties);
+        m_request = compiled_model.create_infer_request();
+    }
+
+    virtual void set_hidden_states(const std::string& tensor_name, ov::Tensor encoder_hidden_states) override
+    {
+        OPENVINO_ASSERT(m_request, "UNet model must be compiled first");
+        m_request.set_tensor(tensor_name, encoder_hidden_states);
+    }
+
+    virtual void set_adapters(AdapterController &adapter_controller, const AdapterConfig& adapters) override
+    {
+        OPENVINO_ASSERT(m_request, "UNet model must be compiled first");
+        adapter_controller.apply(m_request, adapters);
+    }
+
+    virtual ov::Tensor infer(ov::Tensor sample, ov::Tensor timestep) override
+    {
+        OPENVINO_ASSERT(m_request, "UNet model must be compiled first. Cannot infer non-compiled model");
+
+        m_request.set_tensor("sample", sample);
+        m_request.set_tensor("timestep", timestep);
+
+        m_request.infer();
+
+        return m_request.get_output_tensor();
+    }
+
+private:
+
+    ov::InferRequest m_request;
+};
+
+
+}  // namespace genai
+}  // namespace ov

adricwht/genai

src/cpp/src/image_generation/models/unet2d_condition_model.cpp
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "openvino/genai/image_generation/unet2d_condition_model.hpp"
-
+#include "image_generation/models/unet_inference_dynamic.hpp"
 #include <fstream>
 
 #include "json_utils.hpp"
@@ -52,41 +52,25 @@ UNet2DConditionModel& UNet2DConditionModel::reshape(int batch_size, int height, 
     height /= m_vae_scale_factor;
     width /= m_vae_scale_factor;
 
-    std::map<std::string, ov::PartialShape> name_to_shape;
-
-    for (auto && input : m_model->inputs()) {
-        std::string input_name = input.get_any_name();
-        name_to_shape[input_name] = input.get_partial_shape();
-        if (input_name == "timestep") {
-            name_to_shape[input_name][0] = 1;
-        } else if (input_name == "sample") {
-            name_to_shape[input_name] = {batch_size, name_to_shape[input_name][1], height, width};
-        } else if (input_name == "time_ids" || input_name == "text_embeds") {
-            name_to_shape[input_name][0] = batch_size;
-        } else if (input_name == "encoder_hidden_states") {
-            name_to_shape[input_name][0] = batch_size;
-            name_to_shape[input_name][1] = tokenizer_model_max_length;
-        }
-    }
-
-    m_model->reshape(name_to_shape);
+    UNetInference::reshape(m_model, batch_size, height, width, tokenizer_model_max_length);
 
     return *this;
 }
 
 UNet2DConditionModel& UNet2DConditionModel::compile(const std::string& device, const ov::AnyMap& properties) {
     OPENVINO_ASSERT(m_model, "Model has been already compiled. Cannot re-compile already compiled model");
-    ov::Core core = utils::singleton_core();
-    ov::CompiledModel compiled_model;
+
+    m_impl = std::make_shared<UNet2DConditionModel::UNetInferenceDynamic>();
+
     std::optional<AdapterConfig> adapters;
     if (auto filtered_properties = extract_adapters_from_properties(properties, &adapters)) {
         adapters->set_tensor_name_prefix(adapters->get_tensor_name_prefix().value_or("lora_unet"));
         m_adapter_controller = AdapterController(m_model, *adapters, device);
-        compiled_model = core.compile_model(m_model, device, *filtered_properties);
+        m_impl->compile(m_model, device, *filtered_properties);
     } else {
-        compiled_model = core.compile_model(m_model, device, properties);
+        m_impl->compile(m_model, device, properties);
     }
-    m_request = compiled_model.create_infer_request();
+
     // release the original model
     m_model.reset();
 
@@ -94,25 +78,21 @@ UNet2DConditionModel& UNet2DConditionModel::compile(const std::string& device, c
 }
 
 void UNet2DConditionModel::set_hidden_states(const std::string& tensor_name, ov::Tensor encoder_hidden_states) {
-    OPENVINO_ASSERT(m_request, "UNet model must be compiled first");
-    m_request.set_tensor(tensor_name, encoder_hidden_states);
+    OPENVINO_ASSERT(m_impl, "UNet model must be compiled first");
+    m_impl->set_hidden_states(tensor_name, encoder_hidden_states);
 }
 
 void UNet2DConditionModel::set_adapters(const std::optional<AdapterConfig>& adapters) {
-    if (adapters) {
-        m_adapter_controller.apply(m_request, *adapters);
+    OPENVINO_ASSERT(m_impl, "UNet model must be compiled first");
+    if(adapters) {
+        OPENVINO_ASSERT(m_impl, "UNet model must be compiled first");
+        m_impl->set_adapters(m_adapter_controller, *adapters);
     }
 }
 
 ov::Tensor UNet2DConditionModel::infer(ov::Tensor sample, ov::Tensor timestep) {
-    OPENVINO_ASSERT(m_request, "UNet model must be compiled first. Cannot infer non-compiled model");
-
-    m_request.set_tensor("sample", sample);
-    m_request.set_tensor("timestep", timestep);
-
-    m_request.infer();
-
-    return m_request.get_output_tensor();
+    OPENVINO_ASSERT(m_impl, "UNet model must be compiled first. Cannot infer non-compiled model");
+    return m_impl->infer(sample, timestep);
 }
 
 } // namespace genai

src/cpp/src/image_generation/models/unet_inference.hpp
@@ -1,0 +1,99 @@
+// Copyright (C) 2023-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "openvino/genai/image_generation/unet2d_condition_model.hpp"
+
+namespace ov {
+namespace genai {
+
+//REMOVE THIS
+static inline void logBasicModelInfo(const std::shared_ptr<ov::Model>& model) {
+    std::cout << "Model name: " << model->get_friendly_name() << std::endl;
+
+    // Dump information about model inputs/outputs
+    ov::OutputVector inputs = model->inputs();
+    ov::OutputVector outputs = model->outputs();
+
+    std::cout << "\tInputs: " << std::endl;
+    for (const ov::Output<ov::Node>& input : inputs) {
+        const std::string name = input.get_any_name();
+        const ov::element::Type type = input.get_element_type();
+        const ov::PartialShape shape = input.get_partial_shape();
+        const ov::Layout layout = ov::layout::get_layout(input);
+
+        std::cout << "\t\t" << name << ", " << type << ", " << shape << ", " << layout.to_string() << std::endl;
+    }
+
+    std::cout << "\tOutputs: " << std::endl;
+    for (const ov::Output<ov::Node>& output : outputs) {
+        const std::string name = output.get_any_name();
+        const ov::element::Type type = output.get_element_type();
+        const ov::PartialShape shape = output.get_partial_shape();
+        const ov::Layout layout = ov::layout::get_layout(output);
+
+        std::cout << "\t\t" << name << ", " << type << ", " << shape << ", " << layout.to_string() << std::endl;
+    }
+
+    return;
+}
+
+class UNet2DConditionModel::UNetInference {
+
+public:
+    virtual void compile(std::shared_ptr<ov::Model> model, const std::string& device, const ov::AnyMap& properties) = 0;
+    virtual void set_hidden_states(const std::string& tensor_name, ov::Tensor encoder_hidden_states) = 0;
+    virtual void set_adapters(AdapterController& adapter_controller, const AdapterConfig& adapters) = 0;
+    virtual ov::Tensor infer(ov::Tensor sample, ov::Tensor timestep) = 0;
+
+    // utility function to resize model given optional dimensions.
+    static void reshape(std::shared_ptr<ov::Model> model,
+                        std::optional<int> batch_size = {},
+                        std::optional<int> height = {},
+                        std::optional<int> width = {},
+                        std::optional<int> tokenizer_model_max_length = {})
+    {
+        std::map<std::string, ov::PartialShape> name_to_shape;
+        for (auto&& input : model->inputs()) {
+            std::string input_name = input.get_any_name();
+            name_to_shape[input_name] = input.get_partial_shape();
+            if (input_name == "timestep") {
+                name_to_shape[input_name][0] = 1;
+            } else if (input_name == "sample") {
+                if (batch_size) {
+                    name_to_shape[input_name][0] = *batch_size;
+                }
+
+                if (height) {
+                    name_to_shape[input_name][2] = *height;
+                }
+
+                if (width) {
+                    name_to_shape[input_name][3] = *width;
+                }
+            } else if (input_name == "time_ids" || input_name == "text_embeds") {
+                if (batch_size) {
+                    name_to_shape[input_name][0] = *batch_size;
+                }
+            } else if (input_name == "encoder_hidden_states") {
+                if (batch_size) {
+                    name_to_shape[input_name][0] = *batch_size;
+                }
+
+                if (tokenizer_model_max_length) {
+                    name_to_shape[input_name][1] = *tokenizer_model_max_length;
+                }
+            } else if (input_name == "timestep_cond") {
+                if (batch_size) {
+                    name_to_shape[input_name][0] = *batch_size;
+                }
+            }
+        }
+
+        model->reshape(name_to_shape);
+    }
+};
+
+}  // namespace genai
+}  // namespace ov

src/cpp/src/image_generation/models/unet_inference_dynamic.hpp
@@ -1,0 +1,57 @@
+// Copyright (C) 2023-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "image_generation/models/unet_inference.hpp"
+#include "lora_helper.hpp"
+#include "utils.hpp"
+
+namespace ov {
+namespace genai {
+
+
+class UNet2DConditionModel::UNetInferenceDynamic : public UNet2DConditionModel::UNetInference {
+
+public:
+
+    virtual void compile(std::shared_ptr<ov::Model> model, const std::string& device, const ov::AnyMap& properties) override
+    {
+        ov::Core core = utils::singleton_core();
+
+        ov::CompiledModel compiled_model = core.compile_model(model, device, properties);
+        m_request = compiled_model.create_infer_request();
+    }
+
+    virtual void set_hidden_states(const std::string& tensor_name, ov::Tensor encoder_hidden_states) override
+    {
+        OPENVINO_ASSERT(m_request, "UNet model must be compiled first");
+        m_request.set_tensor(tensor_name, encoder_hidden_states);
+    }
+
+    virtual void set_adapters(AdapterController &adapter_controller, const AdapterConfig& adapters) override
+    {
+        OPENVINO_ASSERT(m_request, "UNet model must be compiled first");
+        adapter_controller.apply(m_request, adapters);
+    }
+
+    virtual ov::Tensor infer(ov::Tensor sample, ov::Tensor timestep) override
+    {
+        OPENVINO_ASSERT(m_request, "UNet model must be compiled first. Cannot infer non-compiled model");
+
+        m_request.set_tensor("sample", sample);
+        m_request.set_tensor("timestep", timestep);
+
+        m_request.infer();
+
+        return m_request.get_output_tensor();
+    }
+
+private:
+
+    ov::InferRequest m_request;
+};
+
+
+}  // namespace genai
+}  // namespace ov